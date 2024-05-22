3c182f314612bd7f2d4bbcb7e5e441e1ab9d9ab7
RyanNelson4 <ryannelly414@gmail.com>
2024-05-21 20:39:27 -0400

    Create mine.py

---
+++ mine.py
@@ -0,0 +1,9 @@
+def hello_world():
+    # Print "Hello world!" directly
+    print("Hello world!")
+def hello_world():
+    # Create a variable that has the value of "Hello world!" 
+    variable_name = "Hello world!"
+    
+    # Return the variable
+    return variable_name
